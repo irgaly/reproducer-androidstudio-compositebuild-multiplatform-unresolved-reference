b06dad0decb4c9a32b3027be8c5bcb20d6b9fdbc
irgaly <irgaly@gmail.com>
2023-08-28 12:27:09 +0900

    Update Kotlin 1.8.20

--- compositebuild-library/build.gradle.kts
+++ compositebuild-library/build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    kotlin("multiplatform") version "1.8.10" apply false
+    kotlin("multiplatform") version "1.8.20" apply false
     id("com.android.library") version "7.4.2" apply false
 }
